96c0264332526944b07780cc2d03b5648eb0a007
Chinmayee Lakkad <chinmayee.lakkad@snowflake.com>
2025-09-11 23:21:19 -0400

    corrections

--- src/incident_management/models/landing_zone/incident_attachments.sql
+++ src/incident_management/models/landing_zone/incident_attachments.sql
@@ -12,7 +12,7 @@ select
     to_file('{{ var("docs_stage_path") }}', dm.staged_file_path) as attachment_file,
     dm.event_ts as uploaded_at
 from {{ref('incidents')}} i
-inner join {{ref('doc_metadata')}} dm 
+inner join {{source('landing_zone', 'doc_metadata')}} dm 
 on i.has_attachments 
 and i.reportee_id = dm.user_id 
 and i.external_source_id = dm.channel_id
