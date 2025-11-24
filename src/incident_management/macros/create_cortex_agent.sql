c41d77e8360f6834ef9fe3840423fc0d32767b63
chinmayee.lakkad@snowflake.com <chinmayee.lakkad@snowflake.com>
2025-11-24 04:39:08 +0000

    validating create_cortex_agent.sql

--- src/incident_management/macros/create_cortex_agent.sql
+++ src/incident_management/macros/create_cortex_agent.sql
@@ -1,20 +1,27 @@
 {% macro create_cortex_agent(agent_name, stage_name, spec_file, agent_profile) %}
 
-{% set sql %}
+{% call statement('agent_spec_builder', fetch_result=True) %}
     EXECUTE IMMEDIATE $$
-        BEGIN
-            LET scoped_file_path STRING := BUILD_SCOPED_FILE_URL(@{{ stage_name }}, '{{ spec_file }}');
-            LET agent_spec STRING := INCIDENT_MANAGEMENT.DBT_PROJECT_DEPLOYMENTS.READ_STAGE_FILE(:scoped_file_path);
+    BEGIN
+        LET scoped_file_path STRING := BUILD_SCOPED_FILE_URL(@{{ stage_name }}, '{{ spec_file }}');
+        LET agent_spec STRING := INCIDENT_MANAGEMENT.DBT_PROJECT_DEPLOYMENTS.READ_STAGE_FILE(:scoped_file_path);
+    
+        RETURN agent_spec;           
+    END;
+    $$;
+{% endcall %}
 
-            CREATE OR REPLACE AGENT {{ target.database }}.GOLD_ZONE.{{ agent_name }}
-            PROFILE = '{"display_name": "Incident Management 360", "avatar": "Agent", "color": "green"}'
-            FROM SPECIFICATION
-            ':agent_spec';
-        END;
+{%- set agent_spec = load_result('agent_spec_builder') -%}
+
+{% set cortex_agent_ddl %}
+    CREATE OR REPLACE AGENT {{ target.database }}.GOLD_ZONE.{{ agent_name }}
+    PROFILE = '{"display_name": "Incident Management 360", "avatar": "Agent", "color": "green"}'
+    FROM SPECIFICATION
     $$
-    ;
+    {{ agent_spec['data'][0][0] }}
+    $$;
 {% endset %}
 
-{% do run_query(sql) %}
+{% do run_query(cortex_agent_ddl) %}
 
 {% endmacro %}
