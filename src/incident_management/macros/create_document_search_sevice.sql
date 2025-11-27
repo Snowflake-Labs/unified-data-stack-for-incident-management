0dd385cc3c10422f9530b59c4f5d1229b5d6e069
Chinmayee Lakkad <chinmayee.lakkad@snowflake.com>
2025-11-27 15:36:43 -0500

    Ascii doc updates

--- src/incident_management/macros/create_document_search_sevice.sql
+++ src/incident_management/macros/create_document_search_sevice.sql
@@ -1,7 +1,7 @@
 {% macro create_document_search_service(service_name, search_wh, search_column, target_lag) %}
 
 {% set sql %}
-    CREATE OR REPLACE CORTEX SEARCH SERVICE {{ target.database }}.BRONZE_ZONE.{{ service_name }}
+    CREATE OR REPLACE CORTEX SEARCH SERVICE {{ target.database }}.SILVER_ZONE.{{ service_name }}
       ON {{ search_column }}
       ATTRIBUTES RELATIVE_PATH, EXTENSION
       WAREHOUSE = {{ search_wh }}
